7af92bbb4d21242195b0d4d91a2b5906b52c76fa
JayKumar102030405060707090 <jaydipmore999@gmail.com>
2025-06-07 15:09:48 +0530

    𝐌ᴇᴛᴀ 𝐌ᴜsɪᴄ

---
+++ config.py
@@ -0,0 +1,197 @@
+import re
+from os import getenv
+# ------------------------------------
+# ------------------------------------
+from dotenv import load_dotenv
+from pyrogram import filters
+# ------------------------------------
+# ------------------------------------
+load_dotenv()
+# ------------------------------------
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", None) # youtube song api key, get it from https://t.me/INNOCENCE_FUCKER
+
+# -----------------------------------------------------
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+# ------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN")
+# -------------------------------------------------------
+OWNER_USERNAME = getenv("OWNER_USERNAME","INNOCENT_FUCKER")
+# --------------------------------------------------------
+BOT_USERNAME = getenv("BOT_USERNAME" , "Meta_musicBot")
+# --------------------------------------------------------
+BOT_NAME = getenv("BOT_NAME" , "Meta MUSIC")
+# ---------------------------------------------------------
+ASSUSERNAME = getenv("ASSUSERNAME" , "KomalAssisatnt")
+# ---------------------------------------------------------
+
+
+#---------------------------------------------------------------
+#---------------------------------------------------------------
+MONGO_DB_URI = getenv("MONGO_DB_URI", "monsrv://:@cluster0.")
+#---------------------------------------------------------------
+#---------------------------------------------------------------
+
+# ----------------------------------------------------------------
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
+# ----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+LOGGER_ID = int(getenv("LOGGER_ID",-1002023049910))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", -1002023049910))
+LOG_GROUP_ID = int(getenv("LOGGER_ID",-1002023049910))
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+OWNER_ID = int(getenv("OWNER_ID", 7168729089))
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+# ----------------------------------------------------------------
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/JayKumar102030405060707090/MetaMusic",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+)  # ----------------------------------------------------------------
+# -------------------------------------------------------------------
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+
+
+
+# ------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/KomalMusicUpdate")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Komal_Music_Support")
+# ------------------------------------------------------------------------------# config.py
+
+SUPPORT_GROUP = "https://t.me/Komal_Music_Support"  # Example URL ya string
+adminlist =int(getenv("OWNER_ID", 7168729089))# Tumhare admin IDs ki list
+confirmer = True   # Ya koi bhi default value
+
+# -------------------------------------------------------------------------------
+
+
+
+
+
+
+
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
+AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "9000"))
+SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION", "9999999"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "9999999"))
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "1c21247d714244ddbb09925dac565aed")
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "709e1a2969664491b58200860623ef19")
+# ----------------------------------------------------------------------------------
+
+
+
+
+# -----------------------------------------------------------------------------------
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+# ------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "5242880000"))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
+# --------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
+
+
+
+# ------------------------------------
+# ------------------------------------
+# ------------------------------------
+# ------------------------------------
+STRING1 = getenv("STRING_SESSION", "BQGV228AnXNo28JZUA8Gj_U2NXX7Gf-D7v0wBMBtAVmfm4tchpWtUbg5XaAJqLKMPSVOEseufFuzFrbH3IIit5_IJL997Z_1UUgnAu50mSjHcuDhBLEc1r3Q8T8J506_io6JyLF1ElpuU0jer8QswWQAn2Lp4IGHM0VCaDsSserjVoh1HKwfWQnr3oWnr919gnmRwyDU8TlazWYrcT7MhtMBOLVIn7YOSZsLfMdFu-v0lrujxGWkp48zdTAonetevCBVWbkSetWnRKROhZWUh6ItGtFk5d75uWmtO1LAP_kKlz9wxc-9nsBYrUiDmv4OtSheB8ma_Ag2PKFa9uYZagsdiOhyjAAAAAGwUnlsAA")
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+STRING6 = getenv("STRING_SESSION6", None)
+STRING7 = getenv("STRING_SESSION7", None)
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+
+# ------------------------------------
+# ------------------------------------
+# ------------------------------------
+# ------------------------------------
+
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+START_IMG_URL = getenv(
+    "START_IMG_URL", "https://graph.org/file/d01ee1681b9ac73cdb6bf-c362803186858be64a.jpg"
+)
+PING_IMG_URL = getenv(
+    "PING_IMG_URL", "https://files.catbox.moe/8kifut.jpg"
+)
+PLAYLIST_IMG_URL = "https://files.catbox.moe/2vq8oz.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/8kifut.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/2vq8oz.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/2vq8oz.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/2vq8oz.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/2vq8oz.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/2vq8oz.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/2vq8oz.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/2vq8oz.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/2vq8oz.jpg"
+
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+if SUPPORT_CHANNEL:
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+        )
+
+if SUPPORT_CHAT:
+    if not re.match("(?:http|https)://", SUPPORT_CHAT):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
+        )
+# ---------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
